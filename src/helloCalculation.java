6b01bf7a2a28497607e350575185d86debccfbf5
Anurag Pandey <anurag.pandey@tothenew.com>
2024-08-29 15:36:39 +0530

    third class thir

---
+++ src/helloCalculation.java
@@ -0,0 +1,9 @@
+
+public class helloCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Calculations hould be perform here");
+	}
+
+}
