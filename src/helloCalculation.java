b3d72b35f2111d3151a22828bba4c76ade90d62d
Anurag Pandey <anurag.pandey@tothenew.com>
2024-08-29 15:39:00 +0530

    should spelliing error

--- src/helloCalculation.java
+++ src/helloCalculation.java
@@ -3,7 +3,7 @@ public class helloCalculation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Calculations hould be perform here");
+System.out.println("Calculations should be perform here");
 	}
 
 }
